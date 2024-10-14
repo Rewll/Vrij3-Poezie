8d01221514fce8bcd74f4b2a671e8105570665b5
Meta <Raoulscholtens@gmail.com>
2024-10-14 16:17:27 +0200

    Project geupload en wat spelerbeweging toegevoegd

---
+++ Poezie vrij3/Assets/Eerste vonk prototype/Scripts/SpelerBesturing.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpelerBesturing : MonoBehaviour
+{
+    Rigidbody2D RB;
+    float horizontaal;
+    float verticaal;
+    float moveLimiter = 0.7f;
+
+    public float beweegSnelheid = 20.0f;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontaal = Input.GetAxisRaw("Horizontal"); 
+        verticaal = Input.GetAxisRaw("Vertical"); 
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontaal != 0 && verticaal != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontaal *= moveLimiter;
+            verticaal *= moveLimiter;
+        }
+        RB.velocity = new Vector2(horizontaal * beweegSnelheid, verticaal * beweegSnelheid);
+    }
+}
