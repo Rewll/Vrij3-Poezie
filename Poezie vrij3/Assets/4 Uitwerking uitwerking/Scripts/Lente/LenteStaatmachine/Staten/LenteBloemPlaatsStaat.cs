a42aff04941fb8a77d2391789869d1740ef18270
Meta <Raoulscholtens@gmail.com>
2024-12-09 14:17:45 +0100

    Reapply "bezig met art implementen"
    This reverts commit 72d6065d9145679d868a1e416aac31938f8349d7.

--- Poezie vrij3/Assets/4 Uitwerking uitwerking/Scripts/Lente/LenteStaatmachine/Staten/LenteBloemPlaatsStaat.cs
+++ Poezie vrij3/Assets/4 Uitwerking uitwerking/Scripts/Lente/LenteStaatmachine/Staten/LenteBloemPlaatsStaat.cs
@@ -9,11 +9,22 @@ using UnityEngine.Events;
 
 public class LenteBloemPlaatsStaat : LenteBasisStaat
 {
+    GameManagerOpslag GO;
+
+    public Transform speler1StartPlek;
+    public Transform speler2StartPlek;
+    [Space]
     public GameObject hart;
     public Transform hartPlek;
+    public GameObject hartMetKleur;
+    public GameObject laatsteZonderKleur;
+    [Space]
     public UnityEvent bloemSmeltNaar1;
     public override void OnEnter()
     {
+        GO = GetComponent<GameManagerOpslag>();
+        //GO.verplaatsSpelers(speler1StartPlek.position, speler2StartPlek.position);
+
         hartPlek.gameObject.SetActive(true);
         hart.transform.DOMove(hartPlek.position, 1);
         //Spelers bloemen worden 1 bloem
@@ -28,5 +39,19 @@ public class LenteBloemPlaatsStaat : LenteBasisStaat
     public override void OnExit()
     {
         
+    }
+
+    public void machineStart()
+    {
+        StartCoroutine(machineStartRoutine());
+    }
+
+    IEnumerator machineStartRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+        laatsteZonderKleur.SetActive(false);
+        hartMetKleur.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        //animatie starten 
     }
 }
