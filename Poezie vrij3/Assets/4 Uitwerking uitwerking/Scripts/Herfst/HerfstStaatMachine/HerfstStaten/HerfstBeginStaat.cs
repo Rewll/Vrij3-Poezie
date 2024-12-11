acf2b3abd5961d4d5ccbfa78b469e7ffa1f3dbf4
Meta <Raoulscholtens@gmail.com>
2024-12-11 12:47:49 +0100

    Animatie transitie iets smoother denk ik

--- Poezie vrij3/Assets/4 Uitwerking uitwerking/Scripts/Herfst/HerfstStaatMachine/HerfstStaten/HerfstBeginStaat.cs
+++ Poezie vrij3/Assets/4 Uitwerking uitwerking/Scripts/Herfst/HerfstStaatMachine/HerfstStaten/HerfstBeginStaat.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HerfstBeginStaat : MonoBehaviour
+public class HerfstBeginStaat : HerfstBasisStaat
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnEnter()
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnExit()
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
     }
 }
